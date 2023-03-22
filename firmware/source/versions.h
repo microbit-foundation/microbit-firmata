5079ae816b89dcf65c39582bb144cfe5d64bb0a1
Dr John Vidler <j.vidler@lancaster.ac.uk>
2023-03-22 23:35:47 +0000

    Included new versions.h for new environment variables. Managed by buildv2.py

---
+++ firmware/source/versions.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Update these to sync the major/minor version numbers reported by Firmata
+#define FIRMATA_VERSION_MAJOR 1
+#define FIRMATA_VERSION_MINOR 3
+
+// DO NOT EDIT BELOW THIS LINE (the build script adds stuff!) //
